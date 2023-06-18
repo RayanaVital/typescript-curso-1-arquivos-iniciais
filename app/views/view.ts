e24b6842e15eca28a66e928d279270b4d2c09e7b
RayanaVital <rayanavital@gmail.com>
2023-06-17 22:39:09 -0300

    Implementando método protected feat função para formatar data

--- app/views/view.ts
+++ app/views/view.ts
@@ -10,5 +10,7 @@ export abstract class View<T> {
         this.elemento.innerHTML = this.template(model);
     }
 
-    abstract template(model: T): string;  //metodo abstrato, não precisa ser definida na classe pai, porém precisa ser implementado na classe filha e deve ser definida pela classe filhas
+    protected abstract template(model: T): string;  //metodo abstrato, não precisa ser definida na classe pai, porém precisa ser implementado na classe filha e deve ser definida pela classe filhas
+        //como o método template esta sendo usado dentro do update não há necessidade de ser possivel implementalo no controller, portando o método deve ser protected na classe pai e filhas de forma que não possa ser instanciado 
 }
+
